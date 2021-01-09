eda92b9ab4dd56ea953f49f168cc8e10b6c22ad0
adrianstrecker <azmodea@gmail.com>
2021-01-09 10:01:57 -0600

    Added modified board to index page

---
+++ html/static/js/logic.js
@@ -0,0 +1,38 @@
+// https://codepen.io/pottersson/pen/rusvC
+
+var terrainLetters2Classes = {
+    ' ': 'spacer',
+    '~': 'water',
+    'L': 'lumber',
+    'W': 'wheat',
+    'O': 'ore',
+    'S': 'sheep',
+    'B': 'brick',
+  }
+  
+  var exampleboard = [
+    '  ~~~~',
+    ' ~LWO~',
+    ' ~SBLW~',
+    '~OSBWL~',
+    ' ~BOSL~',
+    ' ~WBL~',
+    '  ~~~~',
+  ];
+  
+  angular.module('catan', []).controller('Board', function Board() {
+    
+    this.board = [];
+    for(var r = 0; r < exampleboard.length; r++) {
+      this.board[r] = [];
+      for(var c = 0; c < exampleboard[r].length; c++) {
+        this.board[r][c] = {
+          class: terrainLetters2Classes[exampleboard[r][c]],
+          letter: exampleboard[r][c],
+        }
+      }
+    }
+    
+  });
+  
+  
